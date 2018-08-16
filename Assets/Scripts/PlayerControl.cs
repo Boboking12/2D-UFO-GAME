2f164eb71a9ba8c0fca604c156c7b20d361f6c49
Boboking12 <41457080+Boboking12@users.noreply.github.com>
2018-08-16 20:00:36 +1000

    current update
    update

---
+++ Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using System.Collections;
+
+//Adding this allows us to access members of the UI namespace including Text.
+using UnityEngine.UI;
+
+public class PlayerControl : MonoBehaviour {
+
+	public float speed;				
+	public Text countText;			
+	public Text winText;			
+
+	private Rigidbody2D rb2d;		
+	private int count;				
+
+
+	void Start()
+	{
+		
+		rb2d = GetComponent<Rigidbody2D> ();
+		
+		count = 0;
+		
+		winText.text = "";
+		
+		SetCountText ();
+	}
+
+	
+	void FixedUpdate()
+	{	
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+
+		float moveVertical = Input.GetAxis ("Vertical");
+		
+		Vector2 movement = new Vector2 (moveHorizontal, moveVertical);
+	
+		rb2d.AddForce (movement * speed);
+	}
+
+	
+	void OnTriggerEnter2D(Collider2D other) 
+	{
+		
+		if (other.gameObject.CompareTag ("PickUp")) 
+		{		
+			other.gameObject.SetActive(false);
+						
+			count = count + 1;
+						
+			SetCountText ();
+		}
+		
+
+	}
+
+	
+	void SetCountText()
+	{
+		
+		countText.text = "Keys Collected: " + count.ToString ();
+
+		
+		if (count >= 12)			
+			winText.text = "You Escaped!";
+	}
+}
